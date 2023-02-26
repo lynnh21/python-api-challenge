37d78def132e5e421e683db73fd2d4426a640599
lynnh21 <114873615+lynnh21@users.noreply.github.com>
2023-02-26 18:02:50 -0500

    My submission of API challenge

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c83c90759bc40b907c9a37be63c714f5"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "e3fa3528fa16420199f91f6e9be9223d"
